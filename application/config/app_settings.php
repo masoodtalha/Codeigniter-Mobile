ccb7f9475a32c21544bde9a9c16baf647ff6aa67
Talha Masood <masoodtalha7@gmail.com>
2013-06-07 00:58:58 +0500

    Codeigniter Mobile Project Started. Config file all set

---
+++ application/config/app_settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$config['App_Name'] = "My First App";
+$config['App_Version']="1.0.0";
+?>
---
+++ application/views/common/header.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Depending on your configurations this header file will be auto-generated
+ * Remember to include this header with all the files you create
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+ <!-- <meta name="viewport" content="initial-scale=1.0, user-scalable=no">!-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <title><?php echo $this->config->item('App_Name');  ?></title>
+  <link rel="stylesheet" href="https://d10ajoocuyu32n.cloudfront.net/mobile/1.3.1/jquery.mobile-1.3.1.min.css">
+  
+  <!-- Extra Codiqa features -->
+  <link rel="stylesheet" href="codiqa.ext.css">
+  
+  <!-- jQuery and jQuery Mobile -->
+  <script src="https://d10ajoocuyu32n.cloudfront.net/jquery-1.9.1.min.js"></script>
+  <script src="https://d10ajoocuyu32n.cloudfront.net/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+
+  <!-- Extra Codiqa features -->
+ <!-- <script src="https://d10ajoocuyu32n.cloudfront.net/codiqa.ext.js"></script>!-->
+   
+</head>
+
+<body>
+    
+    
